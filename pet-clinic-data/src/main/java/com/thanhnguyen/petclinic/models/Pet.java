9342bfcfe4d8e9782bb2285c8329012a1b3f70db
Thanh Nguyen <thanhnguyencs@outlook.com>
2020-06-25 06:50:15 -0500

    add BaseEntity and extends other classes

--- pet-clinic-data/src/main/java/com/thanhnguyen/petclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/com/thanhnguyen/petclinic/models/Pet.java
@@ -2,7 +2,7 @@ package com.thanhnguyen.petclinic.models;
 
 import java.time.Instant;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petTypes;
     private Owner owner;
     private Instant birthDate;
