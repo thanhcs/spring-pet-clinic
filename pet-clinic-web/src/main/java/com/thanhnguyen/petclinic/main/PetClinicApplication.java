f2bc105b61be2dc191192866588e42a358a35059
Thanh Nguyen <thanhnguyencs@outlook.com>
2020-08-28 08:34:47 -0500

    Update to junit 5 and clean up

--- pet-clinic-web/src/main/java/com/thanhnguyen/petclinic/main/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/thanhnguyen/petclinic/main/PetClinicApplication.java
@@ -1,10 +1,10 @@
 package com.thanhnguyen.petclinic.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.thanhnguyen.petclinic.config", "com.thanhnguyen.petclinic.bootstrap"})
 public class PetClinicApplication {
 
